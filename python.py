443d1fa5fe427706c91f5f9ca28b47c3c7dbd198
vulnerbility <madamsetty.avinash20@st.niituniversity.in>
2023-11-06 12:02:55 +0530

    1

---
+++ python.py
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+ 
+
+#  Arthemetic operations
+print(20 / 3)
+print(20//3)
+print (20%3)
+print(20**3)
+
+#name = 'Python for beginners'
+#len()
+#name.upper()
+#name.lower()
+#name.title()
+#name.find()
+#name.replace()
+#'...' in name
+
+
+#name = '1234567890ba'
+#print(len(name))
+#print(name.replace('ba', 'avi'))
+#print('12345' in name)
+
+
+#name = 'zohn'
+#first = 'balaji'
+#last = 'avinash'
+#zero = first + last + ' a coder'
+#minus = f'{name} [{last}] is a coder'
+#print(minus)
+
+#print("My\nname\nis\nBond.", end=" ")
+#print("James Bond.") 
+
+#balaji = 'he is good'
+#print (balaji[3])
+
+#weight= input("Enter into Pounds" )
+#weight_1 = int(weight) * 0.45
+#print (weight_1)
+
+#birth_year = input("Enter your birth year")
+#age = 2023- int(birth_year)
+#print ("Now you are" + age)
+
+#name = input("what is you name")
+#colour = input ("what is your colour")
+#print (name + "like" + colour)
